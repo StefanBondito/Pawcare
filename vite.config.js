ec14f03004b3a9d582092fd639d5dbd17b5a4609
StefanBondito <stefanbondito@gmail.com>
2024-05-29 12:53:09 +0700

    feat: added homepage and fontawesome utils

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+const path = require('path');
 
 export default defineConfig({
     plugins: [
@@ -11,4 +12,16 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            "~fa": path.resolve(
+                __dirname,
+                "node_modules/@fortawesome/fontawesome-free/scss"
+            ),
+            "~dataTables.net-dt": path.resolve(
+                __dirname,
+                "node_modules/dataTables.net-dt"
+            ),
+        },
+    },
 });
