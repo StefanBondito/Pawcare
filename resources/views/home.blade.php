731d2b650b0bf1bba7d83ed4375c5d0676163a5a
StefanBondito <stefanbondito@gmail.com>
2024-05-30 21:29:06 +0700

    feat: added layouts for admin dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,29 +1,30 @@
 @extends('layout')
 @section('content')
 
-<div class="px-4 py-5 my-5 text-center">
-    <img class="d-block mx-auto mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-    <h1 class="display-5 fw-bold text-body-emphasis">Pawcare</h1>
-    <div class="col-lg-6 mx-auto">
-      <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
-      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-        <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
-        <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button>
-      </div>
+<div class="container-home">
+    <div class="landing px-4 py-5 my-5 text-center">
+        <img class="d-block mx-auto mb-4" src="" alt="" width="72" height="57">
+        <h1 class="display-1 custom-color">Pawcare</h1>
+        <div class="col-lg-6 mx-auto">
+            <p class="lead mb-4">We take better care for your pet's everyday needs</p>
+            <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Book Now!</button>
+            </div>
+        </div>
     </div>
 </div>
 
 <div class="container px-4 py-5" id="featured-3">
-    <h2 class="pb-2 border-bottom">Columns with icons</h2>
+    <h2 class="pb-2 border-bottom">What do We Offer at Pawcare?</h2>
     <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
 
       <div class="feature col">
         <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
           <svg class="bi" width="1em" height="1em"><use xlink:href="#collection"></use></svg>
         </div>
-        <h3 class="fs-2 text-body-emphasis">Book Appointment</h3>
-        <p>Create an appointment with your favorite and go to Veterinarian</p>
-        <a href="#" class="btn btn-primary">Book Appointment Now</a>
+        <h3 class="fs-2 text-body-emphasis">Groom</h3>
+        <p>Give your pet a makeover and cleanup to make them fancy</p>
+        <a href="#" class="btn btn-primary">Book Now!</a>
       </div>
 
       <div class="feature col">
@@ -39,12 +40,9 @@
         <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
           <svg class="bi" width="1em" height="1em"><use xlink:href="#toggles2"></use></svg>
         </div>
-        <h3 class="fs-2 text-body-emphasis">Featured title</h3>
-        <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-        <a href="#" class="icon-link">
-          Call to action
-          <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
-        </a>
+        <h3 class="fs-2 text-body-emphasis">Clinic</h3>
+        <p>Pets can get sick, give them a look at our trusty clinics all around</p>
+        <a href="#" class="btn btn-success">Book now!</a>
       </div>
     </div>
   </div>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -17,16 +17,16 @@
                 <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
                   <div class="col-md-3 mb-2 mb-md-0">
                     <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
-                      <svg class="bi" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+                      <img src="/storage/images/assets/15710504898649f93190a5626de1a74c.jpg" alt="" height="50">
                     </a>
                   </div>
 
                   <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
                     <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
-                    <li><a href="#" class="nav-link px-2">Features</a></li>
-                    <li><a href="#" class="nav-link px-2">Pricing</a></li>
-                    <li><a href="#" class="nav-link px-2">FAQs</a></li>
-                    <li><a href="#" class="nav-link px-2">About</a></li>
+                    <li><a href="#" class="nav-link px-2">About Us</a></li>
+                    <li><a href="#" class="nav-link px-2">Petshop</a></li>
+                    <li><a href="#" class="nav-link px-2">Groom</a></li>
+                    <li><a href="#" class="nav-link px-2">Clinic</a></li>
                   </ul>
 
                   <div class="col-md-3 text-end">
