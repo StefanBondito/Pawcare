1bf1505ae745c0c0e8f1c9564915313fac11184a
WeebishName <93817821+WeeblyChar@users.noreply.github.com>
2024-06-06 12:15:24 +0700

    added Model: [AccountType, Permission], Seeder: [AccountType, Permission]. Added import DB in each seeder file. Created Login and Signup php file. Added Login page into web php file.

--- database/seeders/Permission.php
+++ database/seeders/Permission.php
@@ -4,9 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
-class ShoppingCartSeeder extends Seeder
+class Permission extends Seeder
 {
     /**
      * Run the database seeds.
