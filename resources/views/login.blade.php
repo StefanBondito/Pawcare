21721f1fdb80b8b8e8ab0913e82b9b675ae9f419
WeebishName <93817821+WeeblyChar@users.noreply.github.com>
2024-06-07 13:20:13 +0700

    fixed local vite

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
         <!-- !!! THE LINE BELOW IS REQUIRED SO YOU CAN USE BOOTSTRAP !!! -->
         @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+        <!-- !!! THE LINE ABOVE IS REQUIRED SO YOU CAN USE BOOTSTRAP !!! -->
 </head>
 <body>
     <h1>Test Login Preview</h1>
